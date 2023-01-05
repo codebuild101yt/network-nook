efe668d5744bf43ac5c959e7866ee704294b3dd2
codebuild101yt <120342910+codebuild101yt@users.noreply.github.com>
2023-01-05 18:22:15 -0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,22 @@
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+// Get form element
+const signUpForm = document.querySelector('#sign-up-form');
+
+// Add submit event
+signUpForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get email and password values
+  const email = signUpForm['sign-up-email'].value;
+  const password = signUpForm['sign-up-password'].value;
+
+  // Sign up user
+  firebase.auth().createUserWithEmailAndPassword(email, password).then((user) => {
+    // Close sign up modal
+    signUpForm.reset();
+  }).catch((error) => {
+    console.error(error);
+  });
+});
