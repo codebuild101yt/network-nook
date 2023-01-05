397432adbcc11112c21363d9cdefa77fac5a2040
codebuild101yt <120342910+codebuild101yt@users.noreply.github.com>
2023-01-05 07:29:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,72 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
 const database = firebase.database();
 
+// Get form elements
+const loginForm = document.querySelector('#login-form');
+const uploadForm = document.querySelector('#upload-form');
+
+// Add submit event for login form
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get email and password values
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // Sign in user
+  firebase.auth().signInWithEmailAndPassword(email, password).then((user) => {
+    // Hide login form
+    loginForm.style.display = 'none';
+
+    // Show upload form
+    uploadForm.style.display = 'block';
+  }).catch((error) => {
+    console.error(error);
+  });
+});
+
+// Add submit event for upload form
+uploadForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get image URL and caption values
+  const imageUrl = uploadForm['image-url'].value;
+  const caption = uploadForm['caption'].value;
+
+  // Add post to database
+  addPostToDatabase(imageUrl, caption).then(() => {
+    // Clear form
+    uploadForm.reset();
+  }).catch((error) => {
+    console.error(error);
+  });
+});
+
+function addPostToDatabase(imageUrl, caption) {
+  const post = {
+    imageUrl: imageUrl,
+    caption: caption
+  };
+
+  // Get a new key for the post
+  const newPostKey = database.ref().child('posts').push().key;
+
+  // Write the new post data in the posts list
+  const updates = {};
+  updates[`/posts/${newPostKey}`] = post;
+  return database.ref().update(updates);
+}
+
+// Sign user out when they close the tab
+window.addEventListener("beforeunload", function (event) {
+  firebase.auth().signOut();
+});
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const database = firebase.database();
+
 // Get form element
 const loginForm = document.querySelector('#login-form');
 const uploadForm = document.querySelector('#upload-form');
